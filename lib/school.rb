e45d7ebb773cd3feadb3743b4b3f89b373753b62
rbrown3 <rosannabrown@outlook.com>
2018-12-13 18:01:25 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,21 @@ roster= {}
     @name=name
     @roster={}
   end
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  def sort
+    sorted = {}
+    roster.each do |grade,students|
+      sorted[grade]=students.sort
+  end
+   sorted
+  end
+end
  #  def add_student(name,grade)
  #    student = (name,grade)
  #  end
