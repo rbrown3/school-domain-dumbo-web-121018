060479b2f76a9e26e173909dddf7246f0260ecd0
rbrown3 <rosannabrown@outlook.com>
2018-12-13 17:50:45 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+require 'pry'
+class School
+attr_accessor :name,:roster,:student
+roster= {}
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+ #  def add_student(name,grade)
+ #    student = (name,grade)
+ #  end
+ #  student
+ # end
+   # def add_student(student_name, grade)
+   #  @roster
+   # end
+  # end
